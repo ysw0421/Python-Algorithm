376a04f482b3d672fe3b8cef08915a9eb5107c48
Sung Wung, Yeom <yeom4032yeom4032@gmail.com>
2019-02-14 12:59:13 +0900

    Update 2577.py

--- BackJoon/2577.py
+++ BackJoon/2577.py
@@ -1,8 +1,8 @@
 '''
 숫자의 개수
 숏코딩:
-i=input;a=i()*i()*i()
-for i in range(10):print`a`.count(`i`)
+m=1;exec(3*'m*=int(input());')
+for i in range(10):print(str(m).count(str(i)))
 '''
 A= int(input())
 B= int(input())
