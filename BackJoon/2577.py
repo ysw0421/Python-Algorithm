29e5a20e1ddea9d2dc10b08e869ca0bbdfd8510e
Sung Wung, Yeom <yeom4032yeom4032@gmail.com>
2019-02-14 11:46:10 +0900

    Create 2577.py

---
+++ BackJoon/2577.py
@@ -0,0 +1,12 @@
+'''
+숫자의 개수
+숏코딩:
+i=input;a=i()*i()*i()
+for i in range(10):print`a`.count(`i`)
+'''
+A= int(input())
+B= int(input())
+C= int(input())
+result = str(A*B*C)
+for in range(0,10)
+    print(result.count(str(0)))
